8b255222915d345e2acccd61485015f0f438463a
Fatih Altinok <photonia@gmail.com>
2022-05-23 23:38:24 +0300

    Exclude e2e tests from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    exclude: [...(configDefaults.exclude ?? []), "e2e"],
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
