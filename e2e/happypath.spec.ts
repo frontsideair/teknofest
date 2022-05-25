bb7e18de016bd06fec511104171ea6393d9188a3
Fatih Altinok <photonia@gmail.com>
2022-05-25 15:53:49 +0300

    Refactor tests to use page object and fixtures

--- e2e/happypath.spec.ts
+++ e2e/happypath.spec.ts
@@ -1,9 +1,5 @@
-import type { Page } from "@playwright/test";
-import { test, expect } from "@playwright/test";
-
-const LOCATORS = {
-  pageHeader: "role=main >> role=heading[level=2]",
-};
+import type { Locator, Page } from "@playwright/test";
+import { test as base, expect } from "@playwright/test";
 
 type Role = "admin" | "advisor" | "student";
 
@@ -13,154 +9,196 @@ type User = {
   role: Role;
 };
 
-const users: Record<Role, User> = {
-  admin: {
-    email: "admin@teknofest.org",
-    password: "teknofestadmin",
-    role: "admin",
-  },
-  advisor: {
-    email: "fatih@gazi.edu.tr",
-    password: "fatihpassword",
-    role: "advisor",
-  },
-  student: {
-    email: "student@school.k12.tr",
-    password: "studentpassword",
-    role: "student",
-  },
-};
+export class TeknofestPage {
+  readonly page: Page;
+  readonly header: Locator;
+  readonly main: Locator;
+  readonly heading: Locator;
+  readonly users: Record<Role, User> = {
+    admin: {
+      email: "admin@teknofest.org",
+      password: "teknofestadmin",
+      role: "admin",
+    },
+    advisor: {
+      email: "fatih@gazi.edu.tr",
+      password: "fatihpassword",
+      role: "advisor",
+    },
+    student: {
+      email: "student@school.k12.tr",
+      password: "studentpassword",
+      role: "student",
+    },
+  };
+  readonly teamName = "BEST TEAM";
 
-const teamName = "BEST TEAM";
+  private userAssertion(user: User) {
+    switch (user.role) {
+      case "admin":
+        return /admin dashboard/i;
+      case "advisor":
+        return /advisor dashboard/i;
+      case "student":
+        return /student dashboard/i;
+    }
+  }
+
+  constructor(page: Page) {
+    this.page = page;
+    this.header = this.page.locator("role=navigation");
+    this.main = this.page.locator("role=main");
+    this.heading = this.main.locator("role=heading[level=2]");
+  }
+
+  goto(page: "home" | "login" | "dashboard" | string) {
+    switch (page) {
+      case "home":
+        return this.page.goto("/");
+      case "login":
+        return this.page.goto("/login");
+      case "dashboard":
+        return this.page.goto("/dashboard");
+      default:
+        return this.page.goto(page);
+    }
+  }
+
+  submit() {
+    return this.page.keyboard.press("Enter");
+  }
+
+  async login(user: User) {
+    await this.goto("login");
+    await this.main
+      .locator("role=textbox[name=/email address/i]")
+      .type(user.email);
+    await this.main
+      .locator("role=textbox[name=/password/i]")
+      .type(user.password);
+    await this.submit();
+    await expect(this.heading).toHaveText(this.userAssertion(user));
+  }
+
+  async register(user: User) {
+    await this.main
+      .locator("role=textbox[name=/email address/i]")
+      .type(user.email);
+    await this.main
+      .locator("role=textbox[name=/password/i]")
+      .type(user.password);
+    await this.main
+      .locator(
+        `role=radiogroup[name=/role/i] >> role=radio[name=/${user.role}/i]`
+      )
+      .click();
+    await this.submit();
+    await expect(this.heading).toHaveText(this.userAssertion(user));
+  }
+
+  async pickDate(day: number) {
+    return await this.page
+      .locator(`role=table >> role=cell[name=${day}]`)
+      .first()
+      .click();
+  }
+
+  async pickMenuItem(text: string) {
+    await this.page
+      .locator(`role=menu >> role=menuitem[name=/${text}/i]`)
+      .click();
+  }
+}
+
+const test = base.extend<{ tf: TeknofestPage }>({
+  tf: async ({ page }, use) => {
+    const tf = new TeknofestPage(page);
+    await use(tf);
+  },
+});
 
 test.describe("Admin flow", () => {
-  test("should allow admin to login and create contest", async ({ page }) => {
-    await page.goto("/");
-    await page.locator("role=navigation >> role=link[name=/login/i]").click();
-    await page
-      .locator("role=main >> role=textbox[name=/email address/i]")
-      .type(users.admin.email);
-    await page
-      .locator("role=main >> role=textbox[name=/password/i]")
-      .type(users.admin.password);
-    await page.keyboard.press("Enter");
-    await expect(page.locator(LOCATORS.pageHeader)).toHaveText(
-      /admin dashboard/i
-    );
+  test("should allow admin to login and create contest", async ({ tf }) => {
+    await tf.goto("home");
+    await tf.header.locator("role=link[name=/login/i]").click();
+    await tf.main
+      .locator("role=textbox[name=/email address/i]")
+      .type(tf.users.admin.email);
+    await tf.main
+      .locator("role=textbox[name=/password/i]")
+      .type(tf.users.admin.password);
+    await tf.submit();
+    await expect(tf.heading).toHaveText(/admin dashboard/i);
 
-    await page.goto("/dashboard");
-    await page
-      .locator("role=main >> role=link[name=/create new contest/i]")
+    await tf.goto("dashboard");
+    await tf.main.locator("role=link[name=/create new contest/i]").click();
+    await expect(tf.heading).toHaveText(/create new contest/i);
+
+    await tf.main
+      .locator("role=textbox[name=/application date range/i]")
       .click();
-    await page
-      .locator("role=main >> role=textbox[name=/application date range/i]")
-      .click();
-    await page.locator("role=table >> role=cell[name=1]").first().click();
-    await page.locator("role=table >> role=cell[name=15]").click();
-    await page.locator("role=main >> role=button[name=/create/i]").click();
-    await expect(page.locator(LOCATORS.pageHeader)).toHaveText(/contest/i);
+    await tf.pickDate(1);
+    await tf.pickDate(15);
+    await tf.main.locator("role=button[name=/create/i]").click();
+    await expect(tf.heading).toHaveText(/contest/i);
   });
 });
 
 test.describe("Advisor flow", () => {
-  test("should allow advisor to register", async ({ page }) => {
-    await page.goto("/");
-    await page
-      .locator("role=navigation >> role=link[name=/register/i]")
-      .click();
-    await register(page, users.advisor, /advisor dashboard/i);
+  test("should allow advisor to register", async ({ tf }) => {
+    await tf.goto("home");
+    await tf.header.locator("role=link[name=/register/i]").click();
+    await tf.register(tf.users.advisor);
   });
 
-  test("should allow advisor to login and create team", async ({ page }) => {
-    await login(page, users.advisor, /advisor dashboard/i);
+  test("should allow advisor to login and create team", async ({ tf }) => {
+    await tf.login(tf.users.advisor);
 
-    await page
-      .locator("role=main >> role=link[name=/create new team/i]")
-      .click();
-    await page
-      .locator("role=main >> role=textbox[name=/team name/i]")
-      .type(teamName);
-    await page.locator("role=main >> role=button[name=/create/i]").click();
-    await expect(page.locator(LOCATORS.pageHeader)).toContainText(teamName);
+    await tf.main.locator("role=link[name=/create new team/i]").click();
+    await expect(tf.heading).toHaveText(/create new team/i);
+
+    await tf.main.locator("role=textbox[name=/team name/i]").type(tf.teamName);
+    await tf.main.locator("role=button[name=/create/i]").click();
+    await expect(tf.heading).toContainText(tf.teamName);
   });
 
   let inviteLink: string | null = null;
 
-  test("should allow advisor to invite members", async ({ page }) => {
-    await login(page, users.advisor, /advisor dashboard/i);
-    await page
-      .locator("role=main >> role=article", { hasText: teamName })
+  test("should allow advisor to invite members", async ({ tf }) => {
+    await tf.login(tf.users.advisor);
+    await tf.main
+      .locator("role=article", { hasText: tf.teamName })
       .locator("role=link[name=/edit team/i]")
       .click();
-    await expect(page.locator(LOCATORS.pageHeader)).toContainText(teamName);
+    await expect(tf.heading).toContainText(tf.teamName);
 
-    inviteLink = await page
-      .locator("role=main >> text=/inviteCode/")
-      .textContent();
+    inviteLink = await tf.main.locator("text=/inviteCode/").textContent();
   });
 
-  test("should allow invited member to register", async ({ page }) => {
+  test("should allow invited member to register", async ({ tf }) => {
     test.skip(inviteLink === null, "No invite link generated");
-    await page.goto(inviteLink!);
-    await register(page, users.student, /student dashboard/i);
+    await tf.goto(inviteLink!);
+    await tf.register(tf.users.student);
 
-    await page.goto(inviteLink!);
-    await expect(page.locator(LOCATORS.pageHeader)).toHaveText(/join team/i);
+    await tf.goto(inviteLink!);
+    await expect(tf.heading).toHaveText(/join team/i);
 
-    await page.locator("role=main >> role=button[name=/join/i]").click();
-    await expect(page.locator(LOCATORS.pageHeader)).toHaveText(
-      /student dashboard/i
+    await tf.main.locator("role=button[name=/join/i]").click();
+    await expect(tf.heading).toHaveText(/student dashboard/i);
+    await expect(tf.main.locator("role=list >> role=listitem")).toContainText(
+      tf.teamName
     );
-    await expect(
-      page.locator("role=main >> role=list >> role=listitem")
-    ).toContainText(teamName);
   });
 
-  test("should allow advisor to see member", async ({ page }) => {
-    await login(page, users.advisor, /advisor dashboard/i);
-    await page.goto("/team/1");
+  test("should allow advisor to see member", async ({ tf }) => {
+    await tf.login(tf.users.advisor);
+    await tf.goto("/team/1");
 
-    await page
-      .locator("role=main >> role=article", { hasText: users.student.email })
-      .locator("role=button")
+    await tf.main
+      .locator("role=article", { hasText: tf.users.student.email })
+      .locator("role=button[name=/team member actions/i]")
       .click();
-    await page
-      .locator("role=menu >> role=menuitem[name=/remove from team/i]")
-      .click();
-    await expect(page.locator("role=main")).not.toContainText(
-      users.student.email
-    );
+    await tf.pickMenuItem("remove from team");
+    await expect(tf.main).not.toContainText(tf.users.student.email);
   });
 });
-
-async function login(page: Page, user: User, assertion: RegExp = /dashboard/i) {
-  await page.goto("/login");
-  await page
-    .locator("role=main >> role=textbox[name=/email address/i]")
-    .type(user.email);
-  await page
-    .locator("role=main >> role=textbox[name=/password/i]")
-    .type(user.password);
-  await page.keyboard.press("Enter");
-  await expect(page.locator(LOCATORS.pageHeader)).toHaveText(assertion);
-}
-
-async function register(
-  page: Page,
-  user: User,
-  assertion: RegExp = /dashboard/i
-) {
-  await page
-    .locator("role=main >> role=textbox[name=/email address/i]")
-    .type(user.email);
-  await page
-    .locator("role=main >> role=textbox[name=/password/i]")
-    .type(user.password);
-  await page
-    .locator(
-      `role=main >> role=radiogroup[name=/role/i] >> role=radio[name=/${user.role}/i]`
-    )
-    .click();
-  await page.keyboard.press("Enter");
-  await expect(page.locator(LOCATORS.pageHeader)).toHaveText(assertion);
-}
